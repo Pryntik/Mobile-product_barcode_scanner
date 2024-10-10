20116f7bf7895733c488072846dddcd56915d0f5
Alexandre Lemetteil <alexandre.lemetteil@gmail.com>
2024-10-10 17:31:14 +0200

    Ajout de la caméra

--- client/pages/Accueil.tsx
+++ client/pages/Accueil.tsx
@@ -1,11 +1,40 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, { useState } from "react";
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { Button, Text, TouchableOpacity, View, Image } from 'react-native';
+import { cameraStyle } from "../styles/Camera";
 
 const Acceuil = () => {
+    const [facing, setFacing] = useState<CameraType>('back');
+    const [permission, requestPermission] = useCameraPermissions();
+
+    if (!permission) {
+        return <View />;
+    }
+    
+    if (!permission.granted) {
+        return (
+            <View style={cameraStyle.container}>
+                <Text style={cameraStyle.message}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+    
+    function toggleCameraFacing() {
+        setFacing(current => (current === 'back' ? 'front' : 'back'));
+    }
+    
     return (
-        <SafeAreaView>
+        <View style={cameraStyle.container}>
             <Text>Page Accueil</Text>
-        </SafeAreaView>
+            <CameraView style={cameraStyle.camera} facing={facing}>
+            <View style={cameraStyle.buttonContainer}>
+                <TouchableOpacity style={cameraStyle.button} onPress={toggleCameraFacing}>
+                    <Image source={require('../assets/reverse_camera.png')}/>
+                </TouchableOpacity>
+            </View>
+            </CameraView>
+        </View>
     );
 }
 
